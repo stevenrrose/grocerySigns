b313a7c267342c9dc259d6f46948e1cc9f983986
Frédéric Bonnet <f.bonnet@creative-ingenierie.fr>
2015-02-09 11:51:13 +0100

    Added Helvetica template

--- templates.js
+++ templates.js
@@ -119,18 +119,18 @@ var templates = {
 			"FIELD06" : { left: "FIELD03.separator", top: 634, 				right: "width", bottom: 730  }
 		}
 	},
-	"test" : {
+	"Helvetica (Letter)" : {
 		/* Letter */
 		width: 		612,
 		height: 	792,
-		font: 		Raiders,
+		font: 		"Helvetica",
 		maxRatio: 	2,
 		padX: 		10, 
-		padY: 		0,
+		padY: 		10,
 		fields : {
 			"FIELD01" : { left: 0,					top: 77,  right: 612,  bottom: 307  },
 			"FIELD02" : { left: 0,					top: 0,	  right: 612,  bottom: 77,  inverted: true },
-			"FIELD03" : { left: 0,					top: 384, right: 612,  bottom: 538, type: 'price', currency: "$", separator: ".", mainHeight: 408, mainShift: -55 },
+			"FIELD03" : { left: 0,					top: 384, right: 612,  bottom: 538, type: 'price', currency: "$", separator: ".", mainHeight: 408, mainShift: 0 },
 			"FIELD04" : { left: 0,					top: 307, right: 612,  bottom: 384  },
 			"FIELD05" : { left: "FIELD03.separator", top: 538, right: 612, bottom: 634  },
 			"FIELD06" : { left: "FIELD03.separator", top: 634, right: 612, bottom: 730  }
