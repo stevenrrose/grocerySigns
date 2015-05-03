a719a91bc2bbe1868db626debc1176ef4a31a982
Frédéric Bonnet <fredericbonnet@free.fr>
2015-05-03 15:35:51 +0200

    Removed Raiders font, make the templates use SansPosterBold, and reworked templates to fill all available spaces.

--- src/templates.js
+++ src/templates.js
@@ -101,16 +101,15 @@
 var globalMaxLength = 100;
 
 /** Load our fonts. */
-var Raiders = new FontFile("fonts/Raiders.ttf", fontLoaded);
 var SansPosterBold = new FontFile("fonts/SansPosterBold.ttf", fontLoaded);
 var ArialBlack = new FontFile("fonts/ArialBlack.ttf", fontLoaded);
 
 var templates = {
-	"Raiders (Letter)" : {
+	"SansPosterBold (Letter)" : {
 		/* Letter */
 		width: 		612,
 		height: 	792,
-		font: 		Raiders,
+		font: 		SansPosterBold,
 		maxRatio: 	2,
 		padX: 		10, 
 		padY: 		0,
@@ -123,104 +122,70 @@ var templates = {
 			"FIELD06" : { left: "FIELD03.separator", top: 634, right: 612,  bottom: 730  },
 		}
 	},
-	"SansPosterBold (A4)" : {
-		/* A4 */
-		width: 		595,
-		height: 	842,
-		font: 		SansPosterBold,
-		maxRatio: 	2,
-		padX: 		10, 
-		padY: 		0,
-		fields: {
-			"FIELD01" : { left: 0,					 top: "FIELD02.bottom", right: "width", bottom: 307  },
-			"FIELD02" : { left: 0,					 top: 0,	  			right: "width", bottom: 77,  inverted: true },
-			"FIELD03" : { left: 0,					 top: 384, 				right: "width", maxLength: 10, bottom: 538, type: 'price', currency: "$", separator: /[.,]/, mainHeight: 408 },
-			"FIELD04" : { left: 0,					 top: 307, 				right: "width", bottom: 384  },
-			"FIELD05" : { left: "FIELD03.separator", top: 538, 				right: "width", bottom: 634  },
-			"FIELD06" : { left: "FIELD03.separator", top: 634, 				right: "width", bottom: 730  },
-		}
-	},
-	"ArialBlack (Letter)" : {
-		/* Letter */
-		width: 		612,
-		height: 	792,
-		font: 		ArialBlack,
-		maxRatio: 	2,
-		padX: 		10, 
-		padY: 		5,
-		fields: {
-			"FIELD01" : { left: 0,					 top: 77,  right: 612,  bottom: 307  },
-			"FIELD02" : { left: 0,					 top: 0,   right: 612,  bottom: 77,  inverted: true },
-			"FIELD03" : { left: 0,					 top: 384, right: 612,  bottom: 538, maxLength: 10, type: 'price', currency: "$", separator: /[.,]/, mainHeight: 408 },
-			"FIELD04" : { left: 0,					 top: 307, right: 612,  bottom: 384, maxLength: 15 },
-			"FIELD05" : { left: "FIELD03.separator", top: 538, right: 612,  bottom: 634  },
-			"FIELD06" : { left: "FIELD03.separator", top: 634, right: 612,  bottom: 730  },
-		}
-	},
 	"Template 1" : {
 		/* Letter */
 		width: 		612,
 		height: 	792,
-		font: 		ArialBlack,
+		font: 		SansPosterBold,
 		maxRatio: 	2,
 		maxHRatio: 	2,
-		padX: 		5, 
+		padX: 		10, 
 		padY: 		0,
 		fields: {
-			"FIELD01" :	{ left: 120, 					top: 150, 				right: 520, 				bottom: 270 },
-			"FIELD02" : { left: 35, 					top: 55,  				right: 560, 				bottom: 95, 	inverted: true },
-			"FIELD03" : { left: 35,						top: 420, 				right: 540, 				bottom: 590, 	maxLength: 10, type: 'price', currency: "$", separator: /[.,]/, mainHeight: 372 },
-			"FIELD04" : { left: 45, 					top: 280, 				right: 450, 				bottom: 390 },
+			"FIELD01" :	{ left: 80, 					top: 120, 				right: 560, 				bottom: 270 },
+			"FIELD02" : { left: 0,  					top: 0,   				right: 612, 				bottom: 50,  	inverted: true },
+			"FIELD03" : { left: 0,						top: 420, 				right: 612, 				bottom: 590, 	maxLength: 10, type: 'price', currency: "$", separator: /[.,]/, mainHeight: 372 },
+			"FIELD04" : { left: 10, 					top: 280, 				right: 450, 				bottom: 410 },
 			"FIELD05" : { left: "FIELD03.left",			top: "FIELD03.bottom", 	right: "FIELD03.currency",  bottom: 730 },
 			"FIELD06" : { left: "FIELD03.separator", 	top: "FIELD03.bottom", 	right: "FIELD03.right",  	bottom: 730 },
-			"FIELD07" : { left: 60, 					top: 90,  				right: 550, 				bottom: 140 },
-			"FIELD08" : { left: 460, 					top: 290, 				right: 560, 				bottom: 390 },
+			"FIELD07" : { left: 30, 					top: 50,  				right: 582, 				bottom: 110 },
+			"FIELD08" : { left: 470, 					top: 300, 				right: 600, 				bottom: 410 },
 		}
 	},
 	"Template 2" : {
 		/* Letter */
 		width: 		612,
 		height: 	792,
-		font: 		ArialBlack,
+		font: 		SansPosterBold,
 		maxRatio: 	2,
 		maxHRatio: 	2,
-		padX: 		5, 
+		padX: 		10, 
 		padY: 		0,
 		fields: {
-			"FIELD01" :	{ left: 140, 			top: 160, 				right: 545, 				bottom: 280 },
-			"FIELD02" : { left: 35, 			top: 55,  				right: 560, 				bottom: 95, 	inverted: true },
-			"FIELD03" : { left: 15,				top: 410, 				right: 550, 				bottom: 590, 	maxLength: 10, type: 'price', currency: "$", separator: /[.,]/, mainHeight: 330 },
-			"FIELD04" : { left: 140, 			top: 300, 				right: 520, 				bottom: 410 },
-			"FIELD05" : { left: 430, 			top: 610, 				right: 550,  				bottom: 690,	type: 'image' },
-			"FIELD06" : { left: "FIELD03.left",	top: "FIELD03.bottom", 	right: "FIELD03.currency",  bottom: 680 },
-			"FIELD07" : { left: 60, 			top: 90,  				right: 550, 				bottom: 140 },
-			"FIELD08" : { left: 20, 			top: 720, 				right: 550, 				bottom: 760 },
-			"FIELD09" : { left: 30, 			top: 250, 				right: 110, 				bottom: 330,	type: 'image' },
+			"FIELD01" :	{ left: 140, 			top: 130, 				right: 580, 				bottom: 260 },
+			"FIELD02" : { left: 0,  			top: 0,   				right: 612, 				bottom: 50,  	inverted: true },
+			"FIELD03" : { left: 0,				top: 390, 				right: 612, 				bottom: 590, 	maxLength: 10, type: 'price', currency: "$", separator: /[.,]/, mainHeight: 380 },
+			"FIELD04" : { left: 140, 			top: 280, 				right: 560, 				bottom: 390 },
+			"FIELD05" : { left: 470, 			top: 620, 				right: 600,  				bottom: 710,	type: 'image' },
+			"FIELD06" : { left: "FIELD03.left",	top: "FIELD03.bottom", 	right: "FIELD03.currency",  bottom: 700 },
+			"FIELD07" : { left: 30, 			top: 50,  				right: 582, 				bottom: 110 },
+			"FIELD08" : { left: "FIELD03.left",	top: 740, 				right: "FIELD03.right",		bottom: 792 },
+			"FIELD09" : { left: 20, 			top: 220, 				right: 110, 				bottom: 310,	type: 'image' },
 		}
 	},
 	"Template 3" : {
 		/* Letter */
 		width: 		612,
 		height: 	792,
-		font: 		ArialBlack,
+		font: 		SansPosterBold,
 		maxRatio: 	2,
 		maxHRatio: 	2,
 		padX: 		5, 
 		padY: 		0,
 		fields: {
-			"FIELD01.1" : { left: 70, 	top: 140, right: 550, bottom: 180, inputId: "FIELD01", align: 'left', filter: function(text) {return splitWords(text).slice(0, -1).join(" ");} },
-			"STATIC01"	: { left: 45,   top: 200, right: 70,  bottom: 260, type: 'static', text: "\u2022", font: 'Helvetica' },
-			"FIELD01.2" : { left: 70, 	top: 180, right: 550, bottom: 260, inputId: "FIELD01", align: 'left', filter: function(text) {return splitWords(text).slice(-1).join(" ");} },
-			"FIELD02"   : { left: 35, 	top: 55,  right: 560, bottom: 95,  inverted: true },
-			"FIELD03"   : { left: 50, 	top: 420, right: 215, bottom: 760 },
-			"FIELD04.1" : { left: 70, 	top: 270, right: 470, bottom: 310, inputId: "FIELD04", align: 'left', filter: function(text) {return splitWords(text).slice(0, -1).join(" ");} },
-			"STATIC02"	: { left: 45,   top: 330, right: 70,  bottom: 390, type: 'static', text: "\u2022", font: 'Helvetica' },
-			"FIELD04.2" : { left: 70, 	top: 310, right: 450, bottom: 390, inputId: "FIELD04", align: 'left', filter: function(text) {return splitWords(text).slice(-1).join(" ");} },
-			"FIELD05"   : { left: 376, 	top: 420, right: 535, bottom: 760 },
-			"FIELD06"   : { left: 460, 	top: 340, right: 550, bottom: 415, angle: 15, padX: 22, padY: 18, background: new ImageFile("images/bang.png", imageLoaded) },
-			"FIELD07"   : { left: 35, 	top: 100, right: 560, bottom: 135 },
-			"STATIC03"	: { left: 230,  top: 420, right: 376, bottom: 650, type: 'static', text: "$" },
-			"STATIC04"	: { left: 230,  top: 650, right: 376, bottom: 760, type: 'static', text: "FOR" },
+			"FIELD01.1" : { left: 40, 	top: 100, right: 600, bottom: 145, inputId: "FIELD01", align: 'left', filter: function(text) {return splitWords(text).slice(0, -1).join(" ");} },
+			"STATIC01"	: { left: 10,   top: 180, right: 40,  bottom: 240, type: 'static', text: "\u2022", font: 'Helvetica' },
+			"FIELD01.2" : { left: 40, 	top: 145, right: 600, bottom: 240, inputId: "FIELD01", align: 'left', filter: function(text) {return splitWords(text).slice(-1).join(" ");} },
+			"FIELD02"   : { left: 0, 	top: 0,   right: 612, bottom: 50,  inverted: true },
+			"FIELD03"   : { left: 0, 	top: 420, right: 210, bottom: 792 },
+			"FIELD04.1" : { left: 40, 	top: 250, right: 520, bottom: 295, inputId: "FIELD04", align: 'left', filter: function(text) {return splitWords(text).slice(0, -1).join(" ");} },
+			"STATIC02"	: { left: 10,   top: 330, right: 40,  bottom: 390, type: 'static', text: "\u2022", font: 'Helvetica' },
+			"FIELD04.2" : { left: 40, 	top: 295, right: 490, bottom: 390, inputId: "FIELD04", align: 'left', filter: function(text) {return splitWords(text).slice(-1).join(" ");} },
+			"FIELD05"   : { left: 400, 	top: 420, right: 612, bottom: 792 },
+			"FIELD06"   : { left: 505, 	top: 330, right: 605, bottom: 415, angle: 15, padX: 22, padY: 18, background: new ImageFile("images/bang.png", imageLoaded) },
+			"FIELD07"   : { left: 0, 	top: 55,  right: 612, bottom: 95 },
+			"STATIC03"	: { left: 220,  top: 420, right: 390, bottom: 670, type: 'static', text: "$" },
+			"STATIC04"	: { left: 220,  top: 670, right: 390, bottom: 792, type: 'static', text: "FOR" },
 		}
 	},
 };
