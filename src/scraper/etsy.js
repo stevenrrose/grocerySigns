7d2a6502e68a714b533ca49e9a9102931ae381bc
Frédéric Bonnet <fredericbonnet@free.fr>
2016-02-20 01:00:14 +0100

    Added Etsy feedback page scraping

--- src/scraper/etsy.js
+++ src/scraper/etsy.js
@@ -47,34 +47,65 @@ providers["Etsy"] = {
         console.log("fetch", itemId);
         var url = "http://www.etsy.com/listing/" + itemId;
         artoo.ajaxSpider(
-            [{url: fetchUrl, data: {url: url}}],
+            [{url: fetchUrl, data: {url: url}}, {url: fetchUrl, data: {url: url+'/stubs/feedback'}}],
             {
-                scrape: {
-                    params: {limit: 1},
-                    iterator: "#content",
-                    data: {
-                        title: function() {return $(this).parent().find("meta[name='twitter:title']").attr("value");},
-                        price: function() {return $(this).parent().find("meta[name='twitter:data1']").attr("value");},
-                        vendor: {sel: "#seller .shop-name span[itemprop='title']", method: 'text'},
-                        features: {sel: ".properties", scrape: {iterator: "li", data: 'text'}},
-                        description: function() {
-                            return splitSentences(
-                                $.makeArray(
-                                    $(this).find("#description-text").contents()
-                                        .map(function() {
-                                            return $(this).text();
-                                        })
-                                ).join(". ")
-                            ).filter(function(e) {
-                                return e.trim() != "";
-                            });
-                        },
-                        images: {sel: "#image-carousel", scrape: {iterator: "li", data: 'data-large-image-href'}},
+                process: function(data, i, total) {
+                    if (i == 0) {
+                        // Main page.
+                        data = artoo.helpers.jquerify(data);
+                        return artoo.scrape(
+                            data.find("#content"),
+                            {
+                                title: function() {return $(this).parent().find("meta[name='twitter:title']").attr("value");},
+                                price: function() {return $(this).parent().find("meta[name='twitter:data1']").attr("value");},
+                                vendor: {sel: "#seller .shop-name span[itemprop='title']", method: 'text'},
+                                features: {sel: ".properties", scrape: {iterator: "li", data: 'text'}},
+                                description: function() {
+                                    return splitSentences(
+                                        $.makeArray(
+                                            $(this).find("#description-text").contents()
+                                                .map(function() {
+                                                    return $(this).text();
+                                                })
+                                        ).join(". ")
+                                    ).filter(function(e) {
+                                        return e.trim() != "";
+                                    });
+                                },
+                                images: {sel: "#image-carousel", scrape: {iterator: "li", data: 'data-large-image-href'}},
+                            },
+                            {limit: 1}
+                        );
+                    } else {
+                        // Feedback page.
+                        data = artoo.helpers.jquerify(data.html);
+                        return artoo.scrape(
+                            data.find(".feedback-comment"),
+                            function() {
+                                return splitSentences(
+                                    $.makeArray(
+                                        $(this).contents()
+                                            .map(function() {
+                                                return $(this).text();
+                                            })
+                                    ).join(". ")
+                                ).filter(function(e) {
+                                    return e.trim() != "";
+                                });
+                            }
+                        );
                     }
                 }
             },
             function(data) {
                 var info = data[0][0];
+                var feedback = data[1];
+                info.reviews = [];
+                for (var i = 0; i < feedback.length; i++) {
+                    for (var j = 0; j < feedback[i].length; j++) {
+                        info.reviews.push(feedback[i][j]);
+                    }
+                }
                 callback($.extend({success: info ? true : false, itemId: itemId, url: url}, info));
             }
         );
